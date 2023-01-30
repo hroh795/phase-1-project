7bdfc29c40ec12a66a7c8bdfda3cbf902e49581a
Ha Young Roh <hyroh795@gmail.com>
2023-01-30 22:36:38 +0900

    hoverover cat sound text added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-This is myreadme file
+#Cute Cats
+
+//description 
+// what the project do
+//context + link to any reference
+//features or background subsection 
+Cucte Cats single page website is a webpage for having a fun time with cute cats. When a visitor eneters a site 
+
+##
+
+cute cat id 1 url
+https://www.womansday.com/life/g32979681/cute-cat-photos/
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 addEventListener('DOMContentLoaded', (event) => {
 
    const title = document.getElementById("title")
+   const titleDiv = document.getElementById("titleDiv")
+   const more = document.getElementById("more")
+
    
    title.addEventListener("click", addSecondTitle)
 
@@ -8,4 +11,21 @@ addEventListener('DOMContentLoaded', (event) => {
       title.innerHTML = "Cute Cats Are Cute!"
    }
 
+   more.addEventListener("mouseover", meow)
+
+   let imgURL;
+   
+   
+   function meow(){
+      const img = document.createElement("img")
+      fetch("http://localhost:3000/cats")
+      .then(function (response){
+         return response.json();
+      })
+      .then(function(data){
+         let meow = document.createElement("div")
+         meow.innerHTML = "meow"
+         titleDiv.appendChild(meow)
+      })
+   }
 });
